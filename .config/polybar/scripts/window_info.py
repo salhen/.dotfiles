e099982aed2be2d609c3b24bcf2a437e968070f3
Tobey Peters <tobeypeters@hotmail.com>
2020-07-15 15:00:00 -0400

    Updated scripts

--- .config/polybar/scripts/window_info.py
+++ .config/polybar/scripts/window_info.py
@@ -7,8 +7,8 @@
 #	subject to the following conditions:
 #	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 #	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-#	 LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-#	 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 #	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 #	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
@@ -20,44 +20,34 @@ import sys
 def get_window_info(e):
     focused_window = i3.get_tree().find_focused()
     
-    if focused_window.window_class is None:
-        return ''
+    if (focused_window.window_class is None): return ''
     
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'application':
-            return to_camelcase(focused_window.window_class)
+    if (len(sys.argv) > 1) and (sys.argv[1] == 'application'):
+    	return to_camelcase(focused_window.window_class)
     
-    title = focused_window.name
-    
-    title_len = len(title)    
-    title = stripclassnameoff(title, '-')
-    
-    if len(title) == title_len:
-    	title = stripclassnameoff(title, '—')
+    title = stripClassFromTitle(title)
     
     title = title.replace('/home/{}'.format(user), '~')
     title = re.sub(r'@.*:', ':', title)
     
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'title':
-            return title
-    else:
-        return to_camelcase(focused_window.window_class) + ': ' + title
+    if (len(sys.argv) > 1) and (sys.argv[1] == 'title'): return title
 
-def stripclassnameoff(str, character):
+    return to_camelcase(focused_window.window_class) + ': ' + title
+
+def stripClassFromTitle(title):    
 	idx = None
-	for i in range(len(str)): 
-	    if character == str[i]: 
-	        idx = i 
-	buffer = str[:idx] 
-		
-	return buffer
+	
+	for i in range(len(title)): 
+	    if (title[i] == '-') or (title[i] == '—'): idx = i
+	        
+	return title[:idx]
 
 def to_camelcase(str):
     return ' '.join([t.title() for t in str.split()])
 
 def on_window_title(i3, e):
     print(get_window_info(e))
+
     
 i3 = i3ipc.Connection()
 
